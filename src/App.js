57c3ea439946de38db7e2d55e7669cbaddef9836
Commit feito por João Paulo <joao.paulo.lima21@escola.gov.br>
2023-05-15 09:09:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+
+export default function App(){
+  return <h1>PROGRAMANDO EM REACT JS</h1>
+}
+
+
+
+/*
+  function App(){
+    return <h1>PROGRAMANDO EM REACT JS</h1>
+  }
+
+  export default App;
+
+  */
